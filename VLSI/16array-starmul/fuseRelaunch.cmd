05fd19c9638adf22961934f732f4875421e99a57
Hruthik Gurugubelli <hruthikgurugubelli123@gmail.com>
2021-02-18 08:22:32 +0530

    Add files via upload

---
+++ VLSI/16array-starmul/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "/home/hruthik/Desktop/VLSI/sixteenarray/starrmul_isim_beh.exe" -prj "/home/hruthik/Desktop/VLSI/sixteenarray/starrmul_beh.prj" "work.starrmul" "work.glbl" 
